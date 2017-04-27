dde7fa083d07981277bc18895c1e60c83d3937ec
U-DESKTOP-BN5HS2K\Solecki <igor.ps2@gmail.com>
2017-04-27 20:52:03 -0300

    Estética

--- testes/arv/comptmp.bat
+++ testes/arv/comptmp.bat
@@ -2,6 +2,6 @@
 
 rem Compila testes de tempo de execução
 g++ -std=c++11 -o tmp -O3 ^
-    -I%hwloc%\include tst_tmp.cpp ^
+	-I%hwloc%\include tst_tmp.cpp ^
 	-L%hwloc%\lib -lhwloc ^
 	%*
--- testes/arv/comptmp.sh
+++ testes/arv/comptmp.sh
@@ -1,4 +1,4 @@
 # Compila testes de tempo de execução
 g++ -std=c++11 tst_tmp.cpp -o tmp -O3 \
-    $(pkg-config --cflags hwloc) \
+	$(pkg-config --cflags hwloc) \
 	$(pkg-config --libs hwloc)
