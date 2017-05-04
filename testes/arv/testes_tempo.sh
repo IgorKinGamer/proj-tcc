3852f1551fa92d9bcf67df05fcd9d0b416f8e013
Laercio Lima Pilla <laercio.pilla@ufsc.br>
2017-05-04 10:58:39 -0300

    Makefile e múltiplas repetições em script de teste Linux

--- testes/arv/testes_tempo.sh
+++ testes/arv/testes_tempo.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
 IFS=$'\n'
+REPS=30
 
 # Graus com os quais rodará
 for g in $(cat graus_testes.txt)
 do
-	IFS=' '
-	echo -------
-	echo \> ./tmp $* - $g
-	./tmp $* - $g
+    IFS=$'\n'
+    for i in `seq ${REPS}`;
+    do
+        IFS=' '
+        echo --- $i ---
+        echo \> ./tmp $* - $g
+        ./tmp $* - $g
+    done
 done
