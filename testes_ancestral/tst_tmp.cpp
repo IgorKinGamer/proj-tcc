6d2655186b3aad07597bc6706587d45c1f0c8946
U-DESKTOP-BN5HS2K\Solecki <igor.ps2@gmail.com>
2017-05-09 23:19:13 -0300

    Teste de desempenho cria arquivo CSV
    Por enquanto, coloca os resultados no arquivo "resultados.csv".

--- testes_ancestral/tst_tmp.cpp
+++ testes_ancestral/tst_tmp.cpp
@@ -2,6 +2,7 @@
  * Testes de desempenho temporal das funções de ancestral comum
  */
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -29,16 +30,28 @@ const int ALG_SIMPLES  = 0,
           ALG_MATRIZ   = 3,
           ALG_OVERHEAD = 4,
           NUM_ALGS     = 5;
-bool algoritmo[5] = {};
+bool alg_ativo[NUM_ALGS] = {};
+vector<int> algs_ativos;
+string nomes[NUM_ALGS] =
+{
+	"Simples",
+	"Novo",
+	"Hwloc",
+	"Matriz",
+	"Overhead"
+};
+int tamMaxNome = max_element(nomes, nomes+NUM_ALGS,
+	[](string &a, string &b) { return a.size() < b.size(); })->size();
+vector<double> tempos[NUM_ALGS];
 
 int main(int argc, char *argv[])
 {
 	// Graus de https://www.open-mpi.org/projects/hwloc/lstopo/images/16XeonX7400.v1.11.png
 	vector<int> graus({4, 4, 1, 3, 2, 1, 1, 1});
 	int vezesFora = 2, vezesDentro = 3, iteracoes = 10000, aquecimento = 1000;
-	algoritmo[ALG_SIMPLES] = true;
-	algoritmo[ALG_NOVO   ] = true;
-	algoritmo[ALG_HWLOC  ] = true;
+	alg_ativo[ALG_SIMPLES] = true;
+	alg_ativo[ALG_NOVO   ] = true;
+	alg_ativo[ALG_HWLOC  ] = true;
 	
 	int i = 1;
 	string arg;
@@ -53,17 +66,17 @@ int main(int argc, char *argv[])
 			case 'r': vezesDentro = stoi(resto); break; // Repetições de cada algoritmo
 			case 'R': vezesFora   = stoi(resto); break; // Repetições de tudo
 			case 'A': // Algoritmos
-				for (bool &b : algoritmo) b = false;
+				for (bool &b : alg_ativo) b = false;
 				for (char &alg : resto)
 					switch (alg)
 					{
-						case 's': algoritmo[ALG_SIMPLES ] = true; break; // Simples
-						case 'n': algoritmo[ALG_NOVO    ] = true; break; // Novo
-						case 'h': algoritmo[ALG_HWLOC   ] = true; break; // Hwloc
-						case 'm': algoritmo[ALG_MATRIZ  ] = true; break; // Matriz
-						case 'o': algoritmo[ALG_OVERHEAD] = true; break; // Overhead
-						case '*': for (bool &b : algoritmo) b = true; break; // Todos
-						default: cout << "Algoritmo não reconhecido: " << alg << '\n';
+						case 's': alg_ativo[ALG_SIMPLES ] = true; break; // Simples
+						case 'n': alg_ativo[ALG_NOVO    ] = true; break; // Novo
+						case 'h': alg_ativo[ALG_HWLOC   ] = true; break; // Hwloc
+						case 'm': alg_ativo[ALG_MATRIZ  ] = true; break; // Matriz
+						case 'o': alg_ativo[ALG_OVERHEAD] = true; break; // Overhead
+						case '*': for (bool &b : alg_ativo) b = true; break; // Todos
+						default: cerr << "Algoritmo não reconhecido: " << alg << '\n';
 					}
 				break;
 			case '-': // Graus
@@ -72,20 +85,64 @@ int main(int argc, char *argv[])
 					graus.push_back(stoi(argv[i++]));
  				break;
 			default:
-				cout << "Argumento não reconhecido: " + arg << '\n';
+				cerr << "Argumento não reconhecido: " + arg << '\n';
 		}
+	}
+	for (int alg = 0; alg < NUM_ALGS; alg++)
+		if (alg_ativo[alg])
+			algs_ativos.push_back(alg);
+	if (algs_ativos.empty())
+	{
+		cerr << "Nenhum algoritmo especificado\n";
+		return 0;
 	}
 	
 	executarTestes(graus, iteracoes, aquecimento, vezesFora, vezesDentro);
+	
+	// TODO Escrever em um arquivo em uma pasta única
+	// com um arquivo descrevendo a configuração
+	ofstream saida("resultados.csv");
+	if (!saida)
+	{
+		cerr << "Impossível criar o arquivo\n";
+		return 0;
+	}
+	int ult_alg = algs_ativos.back();
+	algs_ativos.pop_back();
+	// Cabeçalho
+	for (const int &alg : algs_ativos)
+		saida << nomes[alg] << ',';
+	saida << nomes[ult_alg] << '\n';
+	// Amostras
+	int numAmostras = vezesFora*vezesDentro;
+	for (int i = 0; i < numAmostras; i++)
+	{
+		for (const int &alg : algs_ativos)
+			saida << tempos[alg][i] << ',';
+		saida << tempos[ult_alg][i] << '\n';
+	}
+	
+	saida.close();
 }
 
 template <class A, class N>
-void testar(BaseTesteAncestralComum<A, N> &tst, const vector<int> &graus, int vezes)
+void testar(BaseTesteAncestralComum<A, N> &tst, int alg, const vector<int> &graus, int vezes)
 {
+	if (!alg_ativo[alg])
+		return;
+	
 	vector<double> resultados;
 	for (auto i = 0; i < vezes; i++)
-		resultados.push_back(tst.executar(graus));
+	{
+		double t = tst.executar(graus);
+		resultados.push_back(t);
+		tempos[alg].push_back(t);
+	}
 	
+	string &nome = nomes[alg];
+	cout << nome << ": ";
+	for (int i = 0; i < tamMaxNome-nome.size(); i++)
+		cout << ' ';
 	for (auto t : resultados)
 		cout << " (" << t << " ms)";
 	cout << '\n';
@@ -103,31 +160,11 @@ void executarTestes(
 	TesteAncestralComumOverhead overhead(iter, iterAquec);
 	for (auto i = 0; i < vezesFora; i++)
 	{
-		if (algoritmo[ALG_SIMPLES])
-		{
-			cout << "Simples: ";
-			testar(simples, graus, vezesDentro);
-		}
-		if (algoritmo[ALG_NOVO])
-		{
-			cout << "Novo:    ";
-			testar(novo, graus, vezesDentro);
-		}
-		if (algoritmo[ALG_HWLOC])
-		{
-			cout << "Hwloc:   ";
-			testar(hwloc, graus, vezesDentro);
-		}
-		if (algoritmo[ALG_MATRIZ])
-		{
-			cout << "Matriz:  ";
-			testar(matriz, graus, vezesDentro);
-		}
-		if (algoritmo[ALG_OVERHEAD])
-		{
-			cout << "Overhead:";
-			testar(overhead, graus, vezesDentro);
-		}
+		testar(simples , ALG_SIMPLES , graus, vezesDentro);
+		testar(novo    , ALG_NOVO    , graus, vezesDentro);
+		testar(hwloc   , ALG_HWLOC   , graus, vezesDentro);
+		testar(matriz  , ALG_MATRIZ  , graus, vezesDentro);
+		testar(overhead, ALG_OVERHEAD, graus, vezesDentro);
 		cout << '\n';
 	}
 }
